2053b6c81759461ee85ad6335fe20faf8fa1d3da
cmliotta <cmliotta@msn.com>
2015-04-02 15:25:51 -0700

    logout odd but rest finished

---
+++ angularails_ch_17/config/application.rb
@@ -0,0 +1,19 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Angularails
+  class Application < Rails::Application
+    config.generators do |g|
+        g.stylesheets false
+        g.helper false
+        g.javascripts false
+        g.test_framework false
+    end
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
